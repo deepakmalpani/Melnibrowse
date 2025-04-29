b995f645eb2b53f54beb7b949148f3d5020a8aba
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-04-29 11:43:56 +0530

    Make connections persistent and read bytes based on content-length headers

--- browser.py
+++ browser.py
@@ -9,6 +9,7 @@ class URL:
         self.file_path = None
         self.inline_html = None
         self.scheme = scheme
+        self.socket = None
         
         assert self.scheme in ["http", "https","file","data",'view-source'] # the browser now supports http, https, file, data
         
@@ -36,7 +37,7 @@ class URL:
         
         self.request_headers = {
             "Host" : self.host,
-            "Connection" : "close",
+            "Connection" : "keep-alive",
             "User-Agent" : "melnibrowse"
         }
     
@@ -46,23 +47,24 @@ class URL:
         return request
     
     def request(self):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP
-            )
+        if self.socket is None: # create a new connection if a socket doesn't exist
+            self.socket = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP
+                )
+            
+            self.socket.connect((self.host, self.port))
         
-        s.connect((self.host, self.port))
-        
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname= self.host)
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                self.socket = ctx.wrap_socket(self.socket, server_hostname= self.host)
 
         request = "GET {} HTTP/1.1\r\n".format(self.path) # we can now use http/1.1 instead of 1.0
         request = self.add_headers(request, self.request_headers)
         request += "\r\n"
-        s.send(request.encode("utf8"))
-        response = s.makefile("r", encoding = "utf8", newline="\r\n") #returns file like object containing every byte received from server and decodes it using utf8
+        self.socket.send(request.encode("utf8"))
+        response = self.socket.makefile("r", encoding = "utf8", newline="\r\n") #returns file like object containing every byte received from server and decodes it using utf8
 
         statusline = response.readline()  # first line in response is a status line
         version, status, explanation = statusline.split(" ",2) 
@@ -70,18 +72,17 @@ class URL:
         response_headers = {}
         while True:
             line = response.readline() # after the status line, comes the headers
-            if line == "\r\n":
-                break
+            if line == "\r\n": break
             header, value = line.split(":",1)
             response_headers[header.casefold()] = value.strip() # convert header key to lower case and strip whitespace from key
 
-            assert "transfer-encoding" not in response_headers # these headers tell us, that the data is being sent in an unusual way, so making sure they are not present
-            assert "content-encoding" not in response_headers
-
-            content = response.read() # the usual way is to get the sent data, then, is everything after the headers
-            s.close()
+        assert "transfer-encoding" not in response_headers # these headers tell us, that the data is being sent in an unusual way, so making sure they are not present
+        assert "content-encoding" not in response_headers
             
-            return content
+        # read bytes equal to the value of content-length response header
+        content = response.read(int(response_headers['content-length'])) # the usual way is to get the sent data, then, is everything after the headers
+        
+        return content
     
     def open_file(self):
         if self.file_path :
